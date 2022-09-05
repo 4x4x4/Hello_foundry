11ea36dac8a0db04be1d53c5e3cd6bd877211780
Parrot Live user <user@localhost.localdomain>
2022-09-05 05:24:05 +0000

    init

--- script/deploy-token.s.sol
+++ script/deploy-token.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@forge-std/src/Script.sol"; 
 import "@src/Token.sol"; 
 
-contract Myscript is Script {
+contract MyScript is Script {
     SuperSayianInu superSayianInu;
 
     function run() external {
